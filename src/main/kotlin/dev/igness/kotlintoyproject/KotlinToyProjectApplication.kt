445ac8c8000c67eb3f34b914a0322229e94f31a7
Jang Jinook <igness37@gmail.com>
2023-07-02 19:01:59 +0900

    feat: 샘플 컨트롤러 등록

--- src/main/kotlin/dev/igness/kotlintoyproject/KotlinToyProjectApplication.kt
+++ src/main/kotlin/dev/igness/kotlintoyproject/KotlinToyProjectApplication.kt
@@ -2,10 +2,19 @@ package dev.igness.kotlintoyproject
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinToyProjectApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinToyProjectApplication>(*args)
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
